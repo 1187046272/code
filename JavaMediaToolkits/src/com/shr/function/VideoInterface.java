9d37bb8cfee81db5a8f0b2007e03fd184e672e97
shr <1187046272@qq.com>
2017-11-30 11:33:02 +0800

    修改注释

--- JavaMediaToolkits/src/com/shr/function/VideoInterface.java
+++ JavaMediaToolkits/src/com/shr/function/VideoInterface.java
@@ -6,19 +6,19 @@ import com.shr.model.Task;
 import com.shr.model.Video;
 
 public interface VideoInterface {
-	//ת��
+	//视频转码
 	public void encode(Task task);
-	//��ȡ��Ƶ������Ϣ
+	//获取视频信息
 	public Video getInfo(Task task);
-	//��������ֻ֧��flv��mp4
+	//flv和mp4流化。。。支持拖拽
 	public void hint(Task task);
-	//��Ƶ����
+	//切片
 	public List<Video> slice(Task task);
-	//��Ƶ����
+	//视频拼接
 	public Video concat(Task task);
-	//ˮӡ
+	//水印
 	public Video waterPrint(Task task);
-	//���л�
+	//画中画
 	public Video vInv(Task task);
 	
 	
